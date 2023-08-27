05e6ef2ad0e2a790c6488d22eb52f2dec7717cfe
Corvettemina <mina.h.hanna@gmail.com>
2023-08-27 17:07:07 -0400

    Revert "update"
    This reverts commit 01deb911741946b9441663747972abc08cf1e21f.

--- src/components/Reusable/SubmitButton.tsx
+++ src/components/Reusable/SubmitButton.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@mantine/core';
 import { IconSend } from '@tabler/icons-react';
-import React, { useState } from 'react';
+import React from 'react';
 import useUi from '../../store/useUi';
 
 interface SubmitButtonProps {
@@ -8,58 +8,19 @@ interface SubmitButtonProps {
   disabled?: boolean;
 }
 
-
-const SubmitButton: React.FC<SubmitButtonProps> = ({ onClick }) => {
+const SubmitButton: React.FC<SubmitButtonProps> = ({ onClick, disabled }) => {
   const { darkMode } = useUi();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const correctPassword = '1234'; // Replace with the actual correct password
-
-  const handlePasswordChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setEnteredPassword(event.target.value);
-  };
-
-  const handleButtonClick = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleModalConfirm = () => {
-    if (enteredPassword === correctPassword) {
-      onClick(); // Perform the action
-      setIsModalOpen(false);
-    }
-  };
-
-  const handleModalCancel = () => {
-    setIsModalOpen(false);
-    setEnteredPassword('');
-  };
 
   return (
-    <div>
-      <Button
-        onClick={handleButtonClick}
-        disabled={isModalOpen}
-        rightIcon={<IconSend size={14} />}
-        color={darkMode ? 'gray' : 'dark'}
-      >
-        Submit
-      </Button>
-      {isModalOpen && (
-        <div>
-          <input
-            type="password"
-            placeholder="Enter password"
-            value={enteredPassword}
-            onChange={handlePasswordChange}
-          />
-          <button onClick={handleModalConfirm}>Confirm</button>
-          <button onClick={handleModalCancel}>Cancel</button>
-        </div>
-      )}
-    </div>
+    <Button
+      onClick={onClick}
+      disabled={disabled}
+      rightIcon={<IconSend size={14} />}
+      color={darkMode ? 'gray' : 'dark'}
+    >
+      Submit
+    </Button>
   );
 };
 
 export default SubmitButton;
-
