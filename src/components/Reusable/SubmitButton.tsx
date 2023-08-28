87a806430c75bd3c1c220bacb125b3164b7b4d77
Corvettemina <mina.h.hanna@gmail.com>
2023-08-27 23:09:07 -0400

    Add password to Submit Button

--- src/components/Reusable/SubmitButton.tsx
+++ src/components/Reusable/SubmitButton.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@mantine/core';
 import { IconSend } from '@tabler/icons-react';
-import React from 'react';
+import React, { useState } from 'react';
 import useUi from '../../store/useUi';
 
 interface SubmitButtonProps {
@@ -8,18 +8,57 @@ interface SubmitButtonProps {
   disabled?: boolean;
 }
 
-const SubmitButton: React.FC<SubmitButtonProps> = ({ onClick, disabled }) => {
+
+
+const SubmitButton: React.FC<SubmitButtonProps> = ({ onClick }) => {
   const { darkMode } = useUi();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const correctPassword = 'yourCorrectPassword'; // Replace with the actual correct password
+
+  const handlePasswordChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalConfirm = () => {
+    if (enteredPassword === correctPassword) {
+      onClick(); // Perform the action
+      setIsModalOpen(false);
+    }
+  };
+
+  const handleModalCancel = () => {
+    setIsModalOpen(false);
+    setEnteredPassword('');
+  };
 
   return (
-    <Button
-      onClick={onClick}
-      disabled={disabled}
-      rightIcon={<IconSend size={14} />}
-      color={darkMode ? 'gray' : 'dark'}
-    >
-      Submit
-    </Button>
+    <div>
+      <Button
+        onClick={handleButtonClick}
+        disabled={isModalOpen}
+        rightIcon={<IconSend size={14} />}
+        color={darkMode ? 'gray' : 'dark'}
+      >
+        Submit
+      </Button>
+      {isModalOpen && (
+        <div>
+          <input
+            type="password"
+            placeholder="Enter password"
+            value={enteredPassword}
+            onChange={handlePasswordChange}
+          />
+          <button onClick={handleModalConfirm}>Confirm</button>
+          <button onClick={handleModalCancel}>Cancel</button>
+        </div>
+      )}
+    </div>
   );
 };
 
