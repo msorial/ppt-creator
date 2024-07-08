752e053827d603b8e87d7e7e0e745487408ac2dc
Corvettemina <mina.h.hanna@gmail.com>
2024-07-07 22:59:02 -0400

    atempt 1

--- src/pages/Vespers.tsx
+++ src/pages/Vespers.tsx
@@ -1,20 +1,32 @@
-import { Checkbox, Flex, Group, Skeleton, Stack, Text } from "@mantine/core";
-import NextButton from "../components/Reusable/NextButton";
-import FormCard from "../components/Reusable/FormCard";
-import PageLayout from "../components/Layout/PageLayout";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import useDates from "../store/useDates";
-import BackButton from "../components/Reusable/BackButton";
-import CardHeader from "../components/Reusable/CardHeader";
-import FormField from "../components/Reusable/FormField";
-import SegControl from "../components/Reusable/SegControl";
-import FormHeader from "../components/Reusable/FormHeader";
-import { hasEmptyValues } from "../lib/functions/hasEmptyValue";
-import { notifications } from "@mantine/notifications";
-import SaveButton from "../components/Reusable/SaveButton";
-import { IconCheck } from "@tabler/icons-react";
+import {
+  Checkbox,
+  Chip,
+  Flex,
+  Group,
+  Input,
+  ScrollArea,
+  Skeleton,
+  Stack,
+  Text,
+} from '@mantine/core';
+import NextButton from '../components/Reusable/NextButton';
+import FormCard from '../components/Reusable/FormCard';
+import PageLayout from '../components/Layout/PageLayout';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import useDates from '../store/useDates';
+import BackButton from '../components/Reusable/BackButton';
+import CardHeader from '../components/Reusable/CardHeader';
+import FormField from '../components/Reusable/FormField';
+import SegControl from '../components/Reusable/SegControl';
+import FormHeader from '../components/Reusable/FormHeader';
+import { hasEmptyValues } from '../lib/functions/hasEmptyValue';
+import { notifications } from '@mantine/notifications';
+import SaveButton from '../components/Reusable/SaveButton';
+import { IconCheck } from '@tabler/icons-react';
+
+import styled from 'styled-components';
 
 export interface VespersApiProps {
   bishop: string;
@@ -53,19 +65,46 @@ const Vespers = () => {
     undefined
   );
   const [vesperOptions, setVesperOptions] = useState<VespersOptionsProps>({
-    bishop: "no",
+    bishop: 'no',
     doxologies: [],
-    gospelLitany: "standard",
-    fiveLitanies: "no",
+    gospelLitany: 'standard',
+    fiveLitanies: 'no',
   });
   const [disabled, setDisabled] = useState<boolean>(true);
 
+  const [optionalDoxologies, setOptionalDoxologies] = useState<string[]>([]);
+
+  const [optionalDoxologiesBoolean, setOptionalDoxologiesBoolean] =
+    useState<string>('true');
+
+  const setoptionalDoxoBoolean = (value: string) => {
+    setOptionalDoxologiesBoolean(value);
+    console.log(optionalDoxologiesBoolean);
+
+    if (optionalDoxologiesBoolean == 'true') {
+      getOptionalDoxologies();
+    }
+  };
+  const setOptionalDoxologiesSelection = (value: string[]) => {
+    console.log(vespersData?.seasonVespersDoxologies);
+  };
+
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  const filteredChips = optionalDoxologies.filter((doxology) =>
+    doxology.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const StyledChip = styled(Chip)`
+    white-space: normal !important;
+    word-wrap: break-word;
+  `;
   // This useEffect returns selections previously made
   useEffect(() => {
-    fetch("https://stmarkapi.com:5000/vespers?date=" + apiDate)
+    fetch('https://stmarkapi.com:5000/vespers?date=' + apiDate)
       .then((response) => response.json())
       .then((data) => {
-        if (data?.status !== "No PPT For this date") {
+        if (data?.status !== 'No PPT For this date') {
           setVesperOptions({
             ...vesperOptions,
             bishop: data?.bishop,
@@ -76,13 +115,13 @@ const Vespers = () => {
         }
       })
       .catch((error) => {
-        console.error("Error fetching API data:", error);
+        console.error('Error fetching API data:', error);
       });
   }, []);
 
   // This useEffect returns ALL options for that given date
   useEffect(() => {
-    fetch("https://stmarkapi.com:8080/vespers?date=" + apiDate)
+    fetch('https://stmarkapi.com:8080/vespers?date=' + apiDate)
       .then((response) => response.json())
       .then((data) => {
         setSelectedCopticDates(data[0]);
@@ -90,9 +129,27 @@ const Vespers = () => {
         setDisabled(false);
       })
       .catch((error) => {
-        console.error("Error fetching API data:", error);
+        console.error('Error fetching API data:', error);
       });
   }, []);
+
+  const getOptionalDoxologies = () => {
+    fetch('http://localhost:8080/optionalDoxologies')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setOptionalDoxologies(data[0]); // Assuming data is an array or object received from the API
+
+        console.log(data[0]); // Logging fetched data
+      })
+      .catch((error) => {
+        console.error('Error fetching API data:', error);
+      });
+  };
 
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
@@ -118,19 +175,19 @@ const Vespers = () => {
       notifications.show({
         withCloseButton: true,
         autoClose: 2000,
-        message: "Please fill in all options",
-        color: "red",
+        message: 'Please fill in all options',
+        color: 'red',
       });
     } else {
       // Modified Copy of Vespers Data to Post to API
       notifications.show({
         withCloseButton: true,
-        message: "Selections Saving",
-        color: "blue",
+        message: 'Selections Saving',
+        color: 'blue',
         loading: true,
-        id: "save",
+        id: 'save',
       });
-      console.log("This message will be logged immediately.");
+      console.log('This message will be logged immediately.');
       const modifiedVespersData = { ...vespersData };
       modifiedVespersData.bishop = vesperOptions.bishop;
       modifiedVespersData.seasonVespersDoxologies = vesperOptions.doxologies;
@@ -139,17 +196,17 @@ const Vespers = () => {
 
       axios
         .post(
-          "https://stmarkapi.com:5000/vespers?date=" + apiDate,
+          'https://stmarkapi.com:5000/vespers?date=' + apiDate,
           modifiedVespersData
         )
         .then(() => {
           notifications.update({
             withCloseButton: true,
             autoClose: 2000,
-            message: "Selections Saved",
-            color: "green",
+            message: 'Selections Saved',
+            color: 'green',
             icon: <IconCheck />,
-            id: "save",
+            id: 'save',
           });
         });
     }
@@ -160,8 +217,8 @@ const Vespers = () => {
       notifications.show({
         withCloseButton: true,
         autoClose: 2000,
-        message: "Please fill in all options",
-        color: "red",
+        message: 'Please fill in all options',
+        color: 'red',
       });
     } else {
       // Modified Copy of Vespers Data to Post to API
@@ -173,14 +230,14 @@ const Vespers = () => {
 
       axios
         .post(
-          "https://stmarkapi.com:5000/vespers?date=" + apiDate,
+          'https://stmarkapi.com:5000/vespers?date=' + apiDate,
           modifiedVespersData
         )
         .then(() => {
           navigate(`/matins`);
         })
         .catch((error) => {
-          console.error("Error submitting data:", error);
+          console.error('Error submitting data:', error);
         });
     }
   };
@@ -192,20 +249,20 @@ const Vespers = () => {
         <FormCard
           content={
             <Flex
-              gap="xl"
-              justify="center"
-              align="flex-start"
-              direction="column"
+              gap='xl'
+              justify='center'
+              align='flex-start'
+              direction='column'
             >
-              <CardHeader header="Vespers" />
+              <CardHeader header='Vespers' />
 
               <FormField
-                title="Bishop Present"
+                title='Bishop Present'
                 options={
                   <SegControl
                     data={[
-                      { label: "No", value: "no" },
-                      { label: "Yes", value: "yes" },
+                      { label: 'No', value: 'no' },
+                      { label: 'Yes', value: 'yes' },
                     ]}
                     value={vesperOptions.bishop}
                     onChange={(value: string) =>
@@ -218,8 +275,8 @@ const Vespers = () => {
                 }
               />
 
-              <Stack align="flex-start" spacing={5}>
-                <Text fz="md" fw={500}>
+              <Stack align='flex-start' spacing={5}>
+                <Text fz='md' fw={500}>
                   Seasonal Vespers Doxologies
                 </Text>
 
@@ -227,12 +284,12 @@ const Vespers = () => {
                   ? vespersData?.seasonVespersDoxologies.map(
                       (item: string, index: number) => (
                         <Checkbox
-                          mt="sm"
+                          mt='sm'
                           key={index}
                           value={item}
                           checked={vesperOptions.doxologies.includes(item)}
                           onChange={handleCheckboxChange}
-                          label={item.split("/").slice(-1)[0].split(".")[0]}
+                          label={item.split('/').slice(-1)[0].split('.pptx')[0]}
                           transitionDuration={0}
                         />
                       )
@@ -242,19 +299,59 @@ const Vespers = () => {
                         height={20}
                         mt={5}
                         width={Math.floor(Math.random() * (100 - 75 + 1)) + 75}
-                        radius="md"
+                        radius='md'
                         key={index}
                       />
                     ))}
               </Stack>
 
               <FormField
-                title="Litany of the Gospel"
+                title='Additional Doxologies'
                 options={
                   <SegControl
                     data={[
-                      { label: "Standard", value: "standard" },
-                      { label: "Alternate", value: "alternate" },
+                      { label: 'No', value: 'true' },
+                      { label: 'Yes', value: 'false' },
+                    ]}
+                    value={optionalDoxologiesBoolean}
+                    onChange={(value) => setoptionalDoxoBoolean(value)}
+                  />
+                }
+              />
+
+              {optionalDoxologiesBoolean === 'false' && (
+                <>
+                  <Input
+                    value={searchQuery}
+                    onChange={(event) =>
+                      setSearchQuery(event.currentTarget.value)
+                    }
+                    placeholder='Search Doxologies...'
+                    w={'100%'}
+                    style={{ marginBottom: '0px' }}
+                  />
+                  <ScrollArea.Autosize h={250}>
+                    <Chip.Group
+                      multiple
+                      onChange={setOptionalDoxologiesSelection}
+                    >
+                      {filteredChips.map((doxology) => (
+                        <StyledChip variant='light' value={doxology} mt={5}>
+                          {doxology.split('/').slice(-1)[0].split('.pptx')[0]}
+                        </StyledChip>
+                      ))}
+                    </Chip.Group>
+                  </ScrollArea.Autosize>
+                </>
+              )}
+
+              <FormField
+                title='Litany of the Gospel'
+                options={
+                  <SegControl
+                    data={[
+                      { label: 'Standard', value: 'standard' },
+                      { label: 'Alternate', value: 'alternate' },
                     ]}
                     value={vesperOptions.gospelLitany}
                     onChange={(value: string) =>
@@ -268,12 +365,12 @@ const Vespers = () => {
               />
 
               <FormField
-                title="Five Short Litanies"
+                title='Five Short Litanies'
                 options={
                   <SegControl
                     data={[
-                      { label: "No", value: "no" },
-                      { label: "Yes", value: "yes" },
+                      { label: 'No', value: 'no' },
+                      { label: 'Yes', value: 'yes' },
                     ]}
                     value={vesperOptions.fiveLitanies}
                     onChange={(value: string) =>
@@ -291,7 +388,7 @@ const Vespers = () => {
       }
       footer={
         <Group>
-          <BackButton onClick={() => navigate("/")} />
+          <BackButton onClick={() => navigate('/')} />
           <NextButton onClick={handleSubmit} disabled={disabled} />
           <SaveButton onClick={Save} disabled={disabled} />
         </Group>
